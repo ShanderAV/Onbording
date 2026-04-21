e9e5c501e5db324ddd30bc86b8432b82386042d7
ShanderAV <ShanderAV@yandex.ru>
2026-04-21 16:13:41 +0300

    16-13 21.04.2026

--- src/main/java/com/company/onboarding/view/user/UserDetailView.java
+++ src/main/java/com/company/onboarding/view/user/UserDetailView.java
@@ -6,15 +6,22 @@ import com.company.onboarding.entity.User;
 import com.company.onboarding.entity.UserStep;
 import com.company.onboarding.view.main.MainView;
 import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.DataManager;
 import io.jmix.core.EntityStates;
 import io.jmix.flowui.Notifications;
+import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.component.UiComponentsGenerator;
 import io.jmix.flowui.component.textfield.TypedTextField;
 import io.jmix.flowui.model.CollectionPropertyContainer;
 import io.jmix.flowui.model.DataContext;
@@ -22,6 +29,8 @@ import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.awt.*;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 import java.util.TimeZone;
@@ -51,6 +60,8 @@ public class UserDetailView extends StandardDetailView<User> {
     private PasswordEncoder passwordEncoder;
 
     private boolean newEntity;
+    @Autowired
+    private UiComponents uiComponents;
 
     @Subscribe
     public void onInit(final InitEvent event) {
@@ -135,4 +146,23 @@ public class UserDetailView extends StandardDetailView<User> {
             }
         }
     }
+
+    @Supply(to = "stepsDataGrid.completed", subject = "renderer")
+    private Renderer<UserStep> stepsDataGridCompletedRenderer() {
+        return new ComponentRenderer<>(userStep -> {
+            // TODO: create suitable component
+            Checkbox checkbox = uiComponents.create(Checkbox.class);
+            checkbox.setValue(userStep.getComletedDate() != null);
+            checkbox.addValueChangeListener( e -> {
+                if (userStep.getComletedDate() == null){
+                    userStep.setComletedDate(LocalDate.now());
+                } else {
+                    userStep.setComletedDate(null);
+                }
+            });
+            return checkbox;
+        });
+    }
+
+
 }
